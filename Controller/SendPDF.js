c13d7f6c5604658c04e668c8a03ad4c8e31542a6
Ramazann15 <75622445+Ramazann15@users.noreply.github.com>
2023-04-29 19:55:25 +0300

    Update SendPDF.js

--- Controller/SendPDF.js
+++ Controller/SendPDF.js
@@ -2,11 +2,11 @@ const fs = require("fs")
 
 
 exports.sendPDF = (req,res)=>{
-    const path = `${__dirname}/../PDF/${req.body.date}-output.pdf`
+    const path = `${__dirname}/../PDF/${req.params.slug}-output.pdf`
     fs.readFile(path,(err,data)=>{
         if (err) {
             console.error(err);
-            return res.status(500).send('Sunucu hatası');
+            return res.status(500).send('Server Error');
           }
 
          
