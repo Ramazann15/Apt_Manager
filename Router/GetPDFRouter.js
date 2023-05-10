35f757d92773aa031230cdee0d0b596fde995272
Ramazann15 <75622445+Ramazann15@users.noreply.github.com>
2023-05-10 17:44:52 +0300

    Update GetPDFRouter.js

--- Router/GetPDFRouter.js
+++ Router/GetPDFRouter.js
@@ -5,6 +5,6 @@ const sendPDF = require("../Controller/SendPDF");
 
 const router = express.Router();
 router.route("/createPDF/:slug").get(getPDF.createPdf);
-router.route("/:slug").get(sendPDF.sendPDF);
+
 
 module.exports = router;
