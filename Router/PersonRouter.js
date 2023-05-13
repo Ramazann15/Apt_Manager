80a9db0f2c946eb9c57465902fd4c28ebb435957
Ramazann15 <75622445+Ramazann15@users.noreply.github.com>
2023-05-13 13:55:06 +0300

    Update PersonRouter.js

--- Router/PersonRouter.js
+++ Router/PersonRouter.js
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.route("/createPerson").post(personController.createPerson);
 router.route("/editperson").put(personController.editPerson);
+router.route("/getPerson").get(personController.getPerson);
 
 module.exports = router;
