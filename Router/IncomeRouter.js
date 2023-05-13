86d5b8e1741bc2b2fd8825f68d7d935ec462671e
Ramazann15 <75622445+Ramazann15@users.noreply.github.com>
2023-05-13 13:54:12 +0300

    Update IncomeRouter.js

--- Router/IncomeRouter.js
+++ Router/IncomeRouter.js
@@ -4,6 +4,6 @@ const incomeController = require("../Controller/IncomeController");
 const router = express.Router();
 
 router.route("/addincome").post(incomeController.addIncome);
-
+router.route("/person/getIncome").post(GetPersonIncome.PersonIncomes);
 
 module.exports = router;
